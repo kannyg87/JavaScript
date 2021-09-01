baecb71ccc2a2725490086ffae5da717469ba2a5
Kanny <kanny.ghafour87@gmail.com>
2021-09-01 12:02:17 -0500

    adding some examples

--- js/function.js
+++ js/function.js
@@ -9,3 +9,38 @@ var calc = function(num1, num2, opration){
 
 }
 console.log(calc(8,5,sub));  
+
+// var outside = 4
+// function testfunction(num){
+//     num+= 22;
+// }
+// var result = testfunction(outside)
+// console.log(outside)
+
+// var object={
+//     num:5
+// };
+// var testfunc = function(obj){
+//     obj.num += 6;
+// }
+// result = testfunc(object)
+// console.log(object.num)
+
+// function varTest() {
+//     var x = 1;
+//     if (x===1) {
+//       var x = 2;  // same variable!
+//       console.log("inside func var",x);  // 2
+//     }
+//     console.log("outside func var",x);  // 2
+//   }
+//   varTest()
+//   function letTest() {
+//     let x = 1;
+//     if (x === 1) {
+//       let x = 2;  // different variable
+//       console.log("inside test let",x);  // 2
+//     }
+//     console.log("inside test let",x);  // 1
+//   }
+//   letTest()
